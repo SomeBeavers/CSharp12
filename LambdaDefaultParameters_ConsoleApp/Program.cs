9d8495c45fa4063f8e49701e1a1574580856ae51
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-06-26 10:40:53 +0200

    Lambda default parameters: add Console App.

---
+++ LambdaDefaultParameters_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+const int myParameter = 1;
+
+var myLambda = (int x = myParameter) => x + 1;
+
+void Test(int x = myParameter)
+{
+}
+
+Console.WriteLine(myLambda);
