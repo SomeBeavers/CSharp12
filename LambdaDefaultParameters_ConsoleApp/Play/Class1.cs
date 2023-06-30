f9f2b8a556cca732d7c2ad44ee7aaf2292f0c80d
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-06-30 11:17:07 +0200

    Lambda default parameters: add minimal API

---
+++ LambdaDefaultParameters_ConsoleApp/Play/Class1.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+using System;
+class Program1
+{
+    public static void Main(string[] args)
+    {
+        var lam = (Delegate        d = lam1) => { };
+        var action = /*caret*/(int x = 5) => { };
+    }
+
+    private const Delegate lam1 = null;
+}
