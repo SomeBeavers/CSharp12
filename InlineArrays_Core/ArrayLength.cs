ca97e83e7f2791f15b3a3631ca5378e50a4883e9
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-10-26 12:09:21 +0200

    UPD

--- InlineArrays_Core/ArrayLength.cs
+++ InlineArrays_Core/ArrayLength.cs
@@ -10,14 +10,20 @@ public class ArrayLength
 
 		foreach (var name in myStruct) Console.WriteLine(name);
 
+		var i1 = myStruct[0];
+		
+		foreach (var i in myStruct)
+		{
+			Console.WriteLine(i.ToString());
+		}
 	}
 }
 
 [StructLayout(LayoutKind.Auto)]
-[InlineArray(10)]
+[InlineArray(Length)]
 public record struct MyStruct(int Age)
 {
-	private const int Length = -10;
+	private const int Length = 10;
 	public static string T = "test";
 
 
