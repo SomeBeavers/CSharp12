050da136a1c83c9f680ef860c5abcc70e8c9fcbd
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-23 14:26:48 +0100

    Collection Expression

---
+++ CollectionExpression_App/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int rt;
+List<int> list = [3, 4, 5, 6, 7, 8, 9, rt];
+list = [];
+List<int> y = [1, 2, ..list];
+[1,2];
