9937d82a5221a436f8ba9b858d112c2f4132c454
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2024-01-25 16:22:51 +0100

    added projects

--- CollectionExpression_App/Program.cs
+++ CollectionExpression_App/Program.cs
@@ -1,7 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int rt;
-List<int> list = [3, 4, 5, 6, 7, 8, 9, rt];
-list = [];
-List<int> y = [1, 2, ..list];
-[1,2];
+//Console.WriteLine("Hello, World!");
+//int rt;
+//List<int> list = [3, 4, 5, 6, 7, 8, 9];
+//list = [];
+//List<int> y = [1, 2, list];
+//[1,2];
+//using System;
+
+string? s = null;
+List<string> list2 = [s];
+
+public class RefClass
+{
+    List<int> list = [1, 2, new ProjectReference.Class1().p, ..new CollectionExpression_Multi.Class1().e];
+    
+}
+
+
---
+++ CollectionExpression_App/UseNuget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollectionExpression_Nuget;
+
+namespace CollectionExpression_App
+{
+    internal class UseNuget
+    {
+        private List<Class1> classes = [new Class1
+            {
+                collection = ["t", "s", "jdk"]
+            }
+        ];
+    }
+}
