83559f19133dab9fe53e0024f3c3bf201b8ff78b
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-25 17:05:51 +0100

    Collection Expression: simple checks

---
+++ CollectionExpression_App/CodeElement/SimpleCollection1.cs
@@ -0,0 +1,37 @@
+﻿namespace CollectionExpression_App.CodeElement;
+
+public class SimpleCollection1
+{
+	public List<InnerClass> list = [new InnerClass { p = 1, e = 2 }, new InnerClass { p = 3, e = 4 }];
+
+	public void Test(params InnerClass[] parameters)
+	{
+	}
+
+	public void UseTest()
+	{
+		IList<object> foo = new List<object>();
+		foo.Add("");
+		foo = new List<object>();
+		//List<object> foo3 = new List<object> { };
+
+		int[] foo2 = { 1, 2, 3 };
+		int[] foo3 = [1, 2, 3];
+		//int[,] foo4 = [(int[])[(int)1, (int)2]];
+		int[,] foo7 = { };
+		int[] foo8 = { };
+		//int[,] foo5 = [1];
+		//int[][] foo6 = [[1, 2], [3, 4]];
+
+
+		Test([new InnerClass { p = 1, e = 2 }, new InnerClass { p = 3, e = 4 }]);
+	}
+}
+
+
+
+public class InnerClass
+{
+	public int e;
+	public int p;
+}
---
+++ CollectionExpression_App/CodeElement/SimpleCollectionWithSpread.cs
@@ -0,0 +1,20 @@
+﻿namespace CollectionExpression_App.CodeElement;
+
+public class SimpleCollectionWithSpread
+{
+	public void Test(List<InnerClass2> parameterList, IList<InnerClass2> anotherCollection)
+	{
+		InnerClass2[] s =
+		[
+			..parameterList, new InnerClass2(), .. anotherCollection, .. anotherCollection, .. anotherCollection,
+			.. anotherCollection, .. anotherCollection, .. anotherCollection, .. anotherCollection
+		];
+		InnerClass2[] s2 = [..parameterList, new InnerClass2(), ..anotherCollection];
+
+		var innerClass2 = anotherCollection;
+	}
+}
+
+public class InnerClass2
+{
+}
