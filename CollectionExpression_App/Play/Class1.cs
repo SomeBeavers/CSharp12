b2c736acc5f184a108f78f4747531369680d8ad2
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-25 16:23:16 +0100

    Upd

---
+++ CollectionExpression_App/Play/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace CollectionExpression_App.Play;
+
+public class Class1
+{
+	
+}
+
+class A(List<int> s)
+{
+}
+
+class B(List<int> s)
+{
+	public void Test()
+	{
+		new A([
+			1,
+			2,
+			3]);
+
+		var a = new A([1, 2, 3]);
+	}
+}
+
+[CollectionBuilder(typeof(A1), "Create")]
+public struct A1<T> : IEnumerable<int>
+{
+	public IEnumerator<int> GetEnumerator()
+	{
+		throw new NotImplementedException();
+	}
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
+}
+
+public static class A1
+{
+	public static A1<T>? Create<T>(ReadOnlySpan<T> items)
+	{
+		return null!;
+	}
+}
+
+public class MyAttribute : Attribute
+{
+	public MyAttribute(IEnumerable a)
+	{
+	}
+}
+
+[CollectionBuilder(typeof(MyCollectionBuilder), nameof(MyCollectionBuilder.Create))]
+class MyCollection
+{
+}
+class MyCollectionBuilder
+{
+	public static MyCollection Create(ReadOnlySpan<long>   items) => throw null;
+	public static MyCollection Create(ReadOnlySpan<string> items) => throw null;
+}
+
+static class Ext
+{
+	public static IEnumerator<long> GetEnumerator(this MyCollection x) => throw null;
+}
+
+class Program11
+{
+	static void Main()
+	{
+		foreach (var l in new MyCollection())
+		{
+			long s = l;
+		}
+
+		MyCollection x1 = [3, 2];
+		MyCollection x2 = new MyCollection();
+		List<int> x3 = new List<int>();
+	}
+}
--- CollectionExpression_App/Program.cs
+++ CollectionExpression_App/Program.cs
@@ -1,7 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int rt;
-List<int> list = [3, 4, 5, 6, 7, 8, 9, rt];
+﻿int rt;
+List<int> list = [3, 4, 5, 6, 7, 8, 9];
 list = [];
 List<int> y = [1, 2, ..list];
-[1,2];
+int[][] x = [[1],[2]];
