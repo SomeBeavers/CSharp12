c4f2efefd4c98cc4f6b366793343ab4f472251de
Liliya Gabitova <liliya.gabitova@jetbrains.com>
2024-01-26 17:43:15 +0100

    Collection Expressions: checks for element - cast

---
+++ CollectionExpression_App/CodeElement/Cast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionExpression_App.CodeElement
+{
+    internal class Cast
+    {
+        public Cast()
+        {
+        }
+
+        public void Test()
+        {
+            List<BaseClass> list = [..(ImmutableArray<BaseClass>)[]];
+            List<BaseClass> list2 = [..new Pes[2] as IEnumerable<BaseClass>];
+            List<List<BaseClass>> list3 = [[], []];
+            //List<Pes> list2 = new() { ((IEnumerable<BaseClass>)new Pes[2]).First() };
+        }
+    }
+
+    public class Pes : BaseClass
+    {
+
+    }
+
+    public class BaseClass
+    {
+    }
+}
