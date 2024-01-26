be357d7ac83e75c777aa826b80c5e3569dd846b6
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-26 10:53:20 +0100

    Collection Expressions: Add simple element checks.

---
+++ CollectionExpression_App/CodeElement/Element1.cs
@@ -0,0 +1,73 @@
+﻿namespace CollectionExpression_App.CodeElement;
+
+public class Element1
+{
+	public const string S = "6";
+
+	private const string V = "1";
+	private static readonly UserDefinedType UserDefinedType;
+	private static readonly UserDefinedType UserDefinedType2;
+
+	static Element1()
+	{
+		UserDefinedType = new UserDefinedType();
+	}
+
+	public void Test(UserDefinedTypeBase userDefinedBase, UserDefinedType2 userDefinedType2, object objectParameter)
+	{
+		// TODO: Simple element int
+		int[] simpleElement1 = [1];
+		int[] simpleElement2 = [1, 2, 3, 4, 5, 6];
+
+		// TODO: const
+		List<string> const1 = ["1", "2", "3", "4", "5", S, V, OtherClass.S];
+
+		// TODO: user defined type
+		Span<UserDefinedTypeBase> userDefinedList =
+			[(UserDefinedTypeBase)userDefinedType2, UserDefinedType, userDefinedBase];
+		UserDefinedTypeBase[] userDefinedList2 =
+		[
+			..userDefinedList, /*..const1,*/ userDefinedBase, /*..null,*/
+			..new[] { new UserDefinedType() }, ..new List<UserDefinedTypeBase>(), ..new UserDefinedTypeBase[] { }
+		];
+		UserDefinedType[] userDefinedList4 = { };
+		var userDefinedList3 = userDefinedList2;
+	}
+}
+
+internal class MyClassS
+{
+	public const string S = "6";
+	public const int IntField = 6;
+
+	public void Test()
+	{
+		var strings = Array.Empty<string>();
+		bool[] s2 = [];
+		bool[] s = [true, false, true, false, ..new bool[] { }, .. Array.Empty<bool>(), ..s2];
+		//
+	}
+}
+
+public class UserDefinedTypeBase
+{
+	public static explicit operator UserDefinedTypeBase(UserDefinedType2 v)
+	{
+		throw new NotImplementedException();
+	}
+}
+
+public class UserDefinedType : UserDefinedTypeBase
+{
+	public string Name { get; set; }
+}
+
+public class UserDefinedType2
+{
+	public string Name { get; set; }
+}
+
+public static class OtherClass
+{
+	public const string S = "6";
+}
--- CollectionExpression_App/CodeElement/SimpleCollectionWithSpread.cs
+++ CollectionExpression_App/CodeElement/SimpleCollectionWithSpread.cs
@@ -2,14 +2,15 @@
 
 public class SimpleCollectionWithSpread
 {
-	public void Test(List<InnerClass2> parameterList, IList<InnerClass2> anotherCollection)
+	public void Test(List<InnerClass2> parameterList, IList<InnerClass2> anotherCollection, InnerClass2 classParameter)
 	{
 		InnerClass2[] s =
 		[
 			..parameterList, new InnerClass2(), .. anotherCollection, .. anotherCollection, .. anotherCollection,
 			.. anotherCollection, .. anotherCollection, .. anotherCollection, .. anotherCollection
 		];
-		InnerClass2[] s2 = [..parameterList, new InnerClass2(), ..anotherCollection];
+		InnerClass2[] s2 = [classParameter, .. parameterList, new InnerClass2(), .. anotherCollection];
+		InnerClass2[] s3 = new InnerClass2[] { };
 
 		var innerClass2 = anotherCollection;
 	}
