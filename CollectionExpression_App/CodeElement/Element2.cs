75d5f8472801d0112a128236b13f3d0a8578e023
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-01-29 10:12:22 +0100

    Collection Expression: add simple generic

---
+++ CollectionExpression_App/CodeElement/Element2.cs
@@ -0,0 +1,20 @@
+﻿namespace CollectionExpression_App.CodeElement;
+
+public class Element2
+{
+	public void Test(int parameter)
+	{
+		string[] s =
+		[
+			$$"""
+			 my {{parameter}}
+			 test
+			 $"{parameter}".Length.ToString()
+			 """,
+			$"{parameter}".Length.ToString(),
+			.. (string[])["","my string"]
+		];
+
+
+	}
+}
---
+++ CollectionExpression_App/CodeElement/Generic1.cs
@@ -0,0 +1,14 @@
+﻿namespace CollectionExpression_App.CodeElement;
+
+public class Generic1<T> where T : struct
+{
+	public List<T> GetList(T parameter)
+	{
+
+		return [parameter, new T(),];
+	}
+}
+
+public class GenericBase
+{
+}
