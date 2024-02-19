7425b6a07f16a16bd6f1aa3016044f6af3d56f2a
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-02-19 14:41:58 +0100

    Collection Expressions: misc.

---
+++ CollectionExpression_App/Play/Play4.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CollectionExpression_App.Play
+{
+    internal class TestClass
+    {
+        public void Test()
+        {
+            RecordCollection recordCollection = [new Pet(), new Pet()];
+        }
+    }
+
+    [CollectionBuilder(typeof(BuilderStruct), nameof(BuilderStruct.CreatenCreate))]
+    public record RecordCollection(string name)
+    {
+        public void Text()
+        { }
+    }
+
+    public class BuilderStruct
+    {
+        public static RecordCollection CreatenCreate(ReadOnlySpan<Pet> items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    static class ExtStruct
+    {
+        public static IEnumerator<Pet> GetEnumerator(this RecordCollection x) => throw null; // Roslyn ignores this tricky boy
+    }
+
+    public class Pet
+    {
+    }
+}
